b19d9e2ca666b236313e54b83a300fb7c1493861
Sangbaek <sangbaek@mit.edu>
2021-01-11 21:49:33 +0900

    upload scripts to be useful for troubleshooting

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # skim16_troubleshoting
+
+To run troubleshooting,
+```
+run-groovy run_printing.groovy /volatile/clas12/rg-a/production/pass0/Sping19/pass1Test2/dst/recon/006715/rec_clas_006715.evio.00590-00594.hipo /volatile/clas12/rg-a/production/pass0/Sping19/pass1Test8/calib/train/skim16/skim16_006715.hipo 
+```
---
+++ run_printing.groovy
@@ -0,0 +1,38 @@
+import org.jlab.jnp.hipo4.data.Event;
+import org.jlab.jnp.hipo4.data.Bank
+import org.jlab.jnp.hipo4.io.HipoReader
+import org.jlab.io.hipo.HipoDataEvent
+import org.jlab.io.hipo.HipoDataSource
+import org.jlab.groot.data.TDirectory
+import org.jlab.jnp.hipo4.data.SchemaFactory
+import org.jlab.jnp.hipo4.data.Schema
+import wagon.DVCSWagon
+/////////////////////////////////////////////////////////////////////
+
+// def problematicRuns = [45040104, 45167466, 90664027, 45282893, 27046060, 27196869, 69221029, 41957238, 139439623, 132085813, 60462487, 107004602, 107025707, 71579058, 128424051, 85423888, 50002416, 93204648, 29114644, 144625985, 32764272, 81704849, 79539897, 46208798, 46297015, 46460162, 46555534, 86511011, 81850085, 81988334]
+def problematicRun = 46208798
+
+args.each{fname->
+  def reader = new HipoReader()
+  reader.open(fname)
+  SchemaFactory schema = reader.getSchemaFactory();
+  Schema recParticle = schema.getSchema("REC::Particle")
+  Schema runConfig = schema.getSchema("RUN::config")
+  def particle = new Bank(recParticle)
+  def config = new Bank(runConfig)
+  def jnp_event = new org.jlab.jnp.hipo4.data.Event()
+  def dvcswagon = new DVCSWagon()
+
+  while(reader.hasNext()) {
+    reader.nextEvent(jnp_event)
+    particle = jnp_event.read(particle)
+    config = jnp_event.read(config)
+    def eventNumber = config.getInt("event", 0)
+    if (eventNumber == problematicRun){
+      println("event "+eventNumber+ " is detected in the file " + fname)
+      particle.show()
+      if (dvcswagon.processDataEvent(jnp_event, schema)) println("It is a DVCS candidate!")
+    }
+  }
+  reader.close()
+}
